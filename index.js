bca97f3bdc064f8da1e63b39f94ae125b15fdd6a
Zachary Sain <zachsain@gmail.com>
2021-11-01 11:28:56 -0500

    Completed again

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+multiply;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+random;
+
+let num3 = 12
+
+let num4 = 3
+
+let mod = num3 / num4;
+
+mod;
+
+let max = Math.max(3,4,5,1,20)
+
+max;
